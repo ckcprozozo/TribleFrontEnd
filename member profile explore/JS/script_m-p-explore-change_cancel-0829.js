0e42e67caf9b29ae1015e83cf323d632e54d3156
ckcprozozo <ckcprozozo@gmail.com>
2017-02-19 20:43:07 +0800

    Trible Member Data
    by Gary

---
+++ member profile explore/JS/script_m-p-explore-change_cancel-0829.js
@@ -0,0 +1,24 @@
+//更改預定 & 取消預定切換
+
+//點擊"更改預定"
+$(document).ready(function(){
+    $(".c-c-change-booking-link").click(function(){
+        $(".c-c-change-b-body").toggle("fast");
+		$(".c-c-cancel-b-body").hide("fast");
+    });
+});
+//點擊"取消預定"
+$(document).ready(function(){
+    $(".c-c-cancel-booking-link").click(function(){
+        $(".c-c-cancel-b-body").toggle("fast");
+		$(".c-c-change-b-body").hide("fast");
+    });
+});
+
+
+/*你的探險 取消預定 radio*/
+$('#c-b-r-reset').click(function(){
+	$('.c-b-r').prop('checked',false);
+	});
+
+
---
+++ personal sheet preview/JS/script_personal_sheet_1024.js
@@ -0,0 +1,11 @@
+﻿$(document).ready(function(){
+    $('[data-toggle="tooltip"]').tooltip();
+});
+
+$(document).ready(function() {
+  $('#p-s-p-media').carousel({
+    pause: true,
+    interval: false,
+  });
+});
+
